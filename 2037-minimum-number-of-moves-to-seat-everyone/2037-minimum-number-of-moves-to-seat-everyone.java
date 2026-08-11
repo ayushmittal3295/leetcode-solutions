02040f45e13e62704bb4d8b6db47068fa96a02aa
Ayush Mittal <mittalayush0212@gmail.com>
2026-08-11 15:50:36 +0530

    Time: 2 ms (96.54%), Space: 46.5 MB (81.19%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
@@ -1,13 +1,40 @@
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
-        Arrays.sort(seats);
-        Arrays.sort(students);
-        int n = seats.length;
-        int m = students.length;
-        int sum=0;
-        for(int i=0;i<n;i++) {
-            sum+=Math.abs(seats[i]-students[i]);
+        int n =seats.length;
+        int max=0;
+        for(int x:seats) {
+            max=Math.max(max,x);
         }
-        return sum;
+
+        for(int x:students) {
+            max=Math.max(max,x);
+        }
+        
+        int pos_seat[]=new int[max+1];
+        int pos_stu[]=new int[max+1];
+
+
+        for(int pos:seats) {
+            pos_seat[pos]++;
+        }
+
+        for(int pos:students) {
+            pos_stu[pos]++;
+        }
+
+        int i=0;
+        int j=0;
+        int moves=0;
+        while(i<=max && j<=max) {
+            if(pos_seat[i]==0) i++;
+            if(pos_stu[j]==0) j++;
+
+            if(i<=max && j<=max && pos_seat[i]!=0 && pos_stu[j]!=0) {
+                moves+=Math.abs(j-i);
+                pos_seat[i]--;
+                pos_stu[j]--;
+            }
+        }
+        return moves;
     }
 }
