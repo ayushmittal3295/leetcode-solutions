8c4287e5e45d7386711e350bf118b5d00f9f691c
Ayush Mittal <mittalayush0212@gmail.com>
2026-09-11 22:53:27 +0530

    Time: 0 ms (100%), Space: 42 MB (70.47%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static int solve(int n, int dp[]) {
+        if(n==0) {
+            return 0;
+        }
+        if(n==1) {
+            return 1;
+        }
+        if(n==2) {
+            return 1;
+        }
+        if(dp[n]!=-1) {
+            return dp[n];
+        }
+
+        dp[n]=solve(n-1,dp)+solve(n-2,dp)+solve(n-3,dp);
+        return dp[n];
+    }
+    public int tribonacci(int n) {
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
+        
+    }
+}
